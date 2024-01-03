b511d3998969ab23c7e6f098d2276a641084d963
Abdurrahman Javat <66177032+DarkStar911@users.noreply.github.com>
2024-01-03 12:32:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Stock information analysis package'
 LONG_DESCRIPTION = 'A package for loading historical stock data, calculating SMA and RSI, and writing results to CSV files.'
 
